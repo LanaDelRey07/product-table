3d93ef35270e66d6792b054413e918ac98f6ed4d
ignacio cardozo <ignaciocardozomorales@gmail.com>
2022-04-22 13:50:56 -0400

    commit del viernes 22 js

--- src/components/ProductRow.jsx
+++ src/components/ProductRow.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import '../styles/ProductRow.css'
+
 function ProductRow() {
+  const producto ='Nexus7'
+  const precio = '199.99'
   return (
-    <div>Hola a todos</div>
+    <div className='product-row'>
+      <span>{producto} </span><span>{`$${precio}`} </span>
+    </div>
   )
 }
 
